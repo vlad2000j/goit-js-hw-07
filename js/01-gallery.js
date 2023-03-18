6c0f26b89a23788f1ecea1034d9d574d93de7896
Житінський Владислав <114184074+vlad2000j@users.noreply.github.com>
2023-03-18 13:13:05 +0200

    HM7

---
+++ js/01-gallery.js
@@ -0,0 +1,69 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+const galleryElements = [];
+
+galleryItems.map(galleryItem => {
+	const div = document.createElement("div");
+	div.classList.add("gallery__item");
+
+	const a = document.createElement("a");
+	a.classList.add("gallery__link");
+	a.href = galleryItem.original;
+
+	const img = document.createElement("img");
+	img.classList.add("gallery__image");
+	img.src = galleryItem.preview;
+	img.dataset.source = galleryItem.original;
+	img.alt = galleryItem.description;
+
+	a.append(img);
+	div.append(a);
+
+	galleryElements.push(div);
+});
+
+gallery.append(...galleryElements);
+
+gallery.addEventListener("click", onGalleryElementClick);
+
+function onGalleryElementClick(event) {
+	event.preventDefault();
+
+	if (event.target.nodeName !== "IMG") {
+		return;
+	}
+	openModal(event.target.dataset.source);
+}
+
+let instanceRef;
+
+function openModal(largeImgUrl) {
+	const instance = basicLightbox.create(
+		`
+		<img src="${largeImgUrl}">
+	`,
+		{
+			onShow: onShowModal,
+			onClose: onCloseModal,
+		},
+	);
+	instance.show();
+	instanceRef = instance;
+}
+
+function onShowModal() {
+	document.addEventListener("keydown", onEscButtonPress);
+}
+
+function onCloseModal() {
+	document.removeEventListener("keydown", onEscButtonPress);
+}
+
+function onEscButtonPress(event) {
+	if (event.key === "Escape") {
+		instanceRef.close();
+	}
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,28 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+const galleryElements = [];
+
+galleryItems.map(galleryItem => {
+	const a = document.createElement("a");
+	a.classList.add("gallery__item");
+	a.href = galleryItem.original;
+
+	const img = document.createElement("img");
+	img.classList.add("gallery__image");
+	img.src = galleryItem.preview;
+	img.alt = galleryItem.description;
+
+	a.append(img);
+
+	galleryElements.push(a);
+});
+
+gallery.append(...galleryElements);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: 250,
+});
